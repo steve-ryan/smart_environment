3fef07e9db8b4762f06c36de396337240ba9b9bd
root <root@localhost.localdomain>
2019-07-02 23:00:41 +0300

    added database

---
+++ backend/sql/smart_environment.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE IF NOT EXISTS smart_environment;
+USE smart_environment;
+
+CREATE TABLE IF NOT EXISTS clients(
+client_id INT(11) NOT NULL AUTO_INCREMENT,
+name VARCHAR(255) NOT NULL,
+phone_no INT(20) NOT NULL,
+email VARCHAR(255) DEFAULT NULL,
+password VARCHAR(255) NOT NULL,
+PRIMARY KEY(client_id)
+
+);
+CREATE TABLE IF NOT EXISTS seedlings(
+seedling_id INT(11) NOT NULL AUTO_INCREMENT,
+s_name VARCHAR(255) NOT NULL,
+price INT(20) NOT NULL ,
+path TEXT NOT NULL,
+description TEXT NOT NULL,
+PRIMARY KEY(seedling_id)
+
+);
+ 
