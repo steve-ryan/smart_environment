d4a61cde7d9837ca15fe01f998159670cc8adf9a
stephen wachira <stevohyoung@gmail.com>
2019-07-04 02:16:34 +0300

    used jquery to load login and register page

--- frontend/js/nav.js
+++ frontend/js/nav.js
@@ -4,3 +4,16 @@ $(document).ready(()=>{
         $("[rel='root']").load("./../html/client.html");
     })
 });
+
+$(document).ready(()=>{
+    $("[rel='register']").on("click",()=>{
+        $("[rel='root']").load("../../backend/php/register.php")
+    })
+});
+
+$(document).ready(()=>{
+    $("[rel='login']").on("click",()=>{
+        $("[rel='root']").load("../../backend/php/login.php")
+    })
+});
+
