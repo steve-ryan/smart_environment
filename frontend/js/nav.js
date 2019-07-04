efb250356586ed91da448a72117034d752a4522f
root <root@localhost.localdomain>
2019-07-04 06:45:46 +0300

    successful calling of login&register pages

--- frontend/js/nav.js
+++ frontend/js/nav.js
@@ -1,6 +1,19 @@
 $(document).ready(()=>{
 
-    $("[rel='home']").on("click",()=>{
-        $("[rel='root']").load("./../html/client.html");
+    $("[rel='register']").on("click",()=>{
+        $("[rel='root']").load("./../html/register.html");
     })
+    $("[rel='login']").on("click",()=>{
+        $("[rel='root']").load("./../html/login.html");
+    })
+
+    $("[rel='backreg']").on("click",(evt)=>{
+        
+        evt.preventDefault();
+        evt.stopPropagation();
+        evt.stopImmediatePropagation();
+        $("[rel='desp']").load("./../html/register.html");
+    });
+
+    
 });
