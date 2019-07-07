e2318c41847cb72bf2e27a745c37257f81c557c8
root <root@localhost.localdomain>
2019-07-07 10:22:10 +0300

    correct iteration of objects in seedlings

--- backend/php/home.php
+++ backend/php/home.php
@@ -13,16 +13,19 @@ if ($conn->connect_error) {
 $quer="SELECT * FROM seedlings";
 
 $result=$conn->query($quer);
-$rows=$result->fetch_assoc();
-
+$rows=$result->fetch_all(MYSQLI_ASSOC);
+ $y=0;
+ //while($result->num_rows>0){
+// while($y){
 $json_array=array();
 
 
 $json_array[]=$rows;
-   
-  
- echo json_encode($json_array);
+echo json_encode($json_array);
+  //$y++;
+ //}
 
+ 
 //echo $rows['category'];
 
 ?>
