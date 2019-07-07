cc9eb037c3bab654492bdf0dd352dc594c8c1c17
stephen wachira <stevohyoung@gmail.com>
2019-07-07 14:37:25 +0300

    added on click on home

--- frontend/js/nav.js
+++ frontend/js/nav.js
@@ -1,4 +1,4 @@
-$(document).ready(()=>{
+$(document).ready(() => {
 
     // $("[rel='register']").on("click",()=>{
     //     $("[rel='root']").load("./../html/register.html");
@@ -6,15 +6,20 @@ $(document).ready(()=>{
     // $("[rel='login']").on("click",()=>{
     //     $("[rel='root']").load("./../html/login.html");
     // })
-    
 
-    $("[rel='register']").on("click",()=>{
-        $("[rel='root']").load("./../../backend/php/register_clients.php")
+
+    $("[rel='register']").on("click", () => {
+        $("[rel='root']").load("./backend/php/register_clients.php")
     })
 
-    $("[rel='login']").on("click",()=>{
-        $("[rel='root']").load("./../../backend/php/login_client.php")
+    $("[rel='login']").on("click", () => {
+        $("[rel='root']").load("./backend/php/login_client.php")
     })
+
+    $("[rel='home']").on("click", () => {
+        $("[rel='root']").load("./backend/php/home_client.php")
+    })
+
 });
 
 
