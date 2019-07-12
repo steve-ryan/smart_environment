16555994927873687594c74e0489f25167eb872a
stephen wachira <stevohyoung@gmail.com>
2019-07-12 11:33:53 +0300

    renaming of home.php to fruit.php

---
+++ backend/php/fruit_client.php
@@ -0,0 +1,21 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Medicinal plants</title>
+    <link rel="stylesheet" href="./../../frontend/css/client.css">
+    <script src="./../../frontend/js/jq.js"></script>
+    <script src="./../../frontend/js/home.js"></script>
+</head>
+<body>
+    
+        <div rel="item-head" class="item-head"></div>
+        <div rel="root-h" class="root-h"></div>
+        
+
+</body>
+</html>
--- frontend/js/home.js
+++ frontend/js/home.js
@@ -1,53 +1,7 @@
 $(document).ready(()=>{
-       
-    
-
-//     $.get("http://localhost/smart/backend/php/home.php")
-//     .then((data)=>{
-
-//         const obj=JSON.parse(data);
-//         //$("[rel='root']").html(data);
-           
-//         // let x=0;
-//         // let y =obj.length;
-//          console.log(obj[0][1])
-    
-//     $("[rel='item-head']").append(`<h1 class="category-head">FRUIT TREES</h1>`);
-//    // while(x<y){
-//          obj.map(item=>{
-//             let img=item.path;
-//             let name=item.s_name;
-//             let details=item.description;
-//             let come ="https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.kO0LY2D7pCGQoHnyur0s2wHaEK%26pid%3DApi&f=1"
-
-//             console.log(item[0]);
-//             console.log(item.s_name);
-//             // $.each(item,()=>{
-//             return(
-                
-//                 $("[rel='root']").append(
-                    
-//                     '<img src='+img+` alt="image not found"></br>`+
-//                     `<p><h3>`+name+`</h3></p> </br>`+
-//                     `<input type="button" rel="more-details" value="MORE DETAILS" class="more-details"/></br></br>`+
-//                     `<input type="button" rel="buy" value="BUY" class="buy"/>\n`
-//                     ));
-//                // });
-            
-//         });
-    
-//        // x++;
-//     //}
-//         //$("[rel='root']").append("Your name is"+came);
-
-//         // console.log(obj);
-//         // console.log(came);
-//     })
-//     .fail((error)=>{console.error(error)})
-//     .always(()=>{console.log("done")})
     $.ajax({
        type: "get",
-       url: "./../../backend/php/home.php",
+       url: "./../../backend/php/fruit.php",
        dataType: "text",
        success: (data) =>{
 
